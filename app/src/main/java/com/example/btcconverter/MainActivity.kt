af7699064ef1cf185c1c804ab73ea1a3087243c9
Chris-eminence <emichris380@gmail.com>
2021-09-08 23:14:08 +0100

    working on switch button

--- app/src/main/java/com/example/btcconverter/MainActivity.kt
+++ app/src/main/java/com/example/btcconverter/MainActivity.kt
@@ -2,26 +2,57 @@ package com.example.btcconverter
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
+import com.example.btcconverter.databinding.ActivityMainBinding
+import java.text.NumberFormat
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        val btcValue : Double = 0.00000004
-        val mEditAmountInNaira = findViewById<EditText>(R.id.amount_text_box)
-        val mConvertBTN = findViewById<Button>(R.id.convert_btn)
-        val mConvertedAmount = findViewById<TextView>(R.id.conertedAmount)
 
-        mConvertBTN.setOnClickListener {
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-            val valueToConvert: Double = mEditAmountInNaira.getText().toString().toDouble()
-            val result: Double = valueToConvert * btcValue
-            val decimalPlace = String.format("%.2f", result)
-            mConvertedAmount.text = decimalPlace
+
+        binding.convertBtn.setOnClickListener {
+            calculateBTC()
         }
+
+    }
+
+    private fun calculateBTC() {
+        val btcValue: Double = 0.00000004
+        val convertAmount = binding.amountTextBox.text.toString().toDouble()
+//        val decimalPlace = String.format("%.2f", result)
+//        binding.conertedAmount.text = decimalPlace
+        var result = convertAmount * btcValue
+        var roundUp = binding.switchUp.isChecked
+        if (roundUp){
+//            result.toString().toInt()
+            val result = String.format("%.1f", result)
+//            result = kotlin.math.ceil(result)
+        }
+        var formattedResult = NumberFormat.getCurrencyInstance().format(result)
+        binding.conertedAmount.text = formattedResult
+
+
     }
 }
+//        val mEditAmountInNaira = findViewById<EditText>(R.id.amount_text_box)
+//        val mConvertBTN = findViewById<Button>(R.id.convert_btn)
+//        val mConvertedAmount = findViewById<TextView>(R.id.conertedAmount)
+//
+//        mConvertBTN.setOnClickListener {
+//
+//            val valueToConvert: Double = mEditAmountInNaira.getText().toString().toDouble()
+//            val result: Double = valueToConvert * btcValue
+//            val decimalPlace = String.format("%.2f", result)
+//            mConvertedAmount.text = decimalPlace
+//        }
+//    }
+//}
